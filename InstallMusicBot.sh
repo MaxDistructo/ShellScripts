d8bcad2baca3ac557356562c105a295f01789b6e
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-05-03 15:06:06 -0500

    Will now create custom configs
    - Added user input for creating custom configs.
    - Can create both custom config for the Bot
    - NOT TESTED (Semicolons might mess up echo command)
    - Will Run Bot after finishing setup

--- InstallMusicBot.sh
+++ InstallMusicBot.sh
@@ -25,7 +25,12 @@ git clone https://github.com/Just-Some-Bots/MusicBot.git MusicBot -b master
 cd MusicBot
 sudo -H pip3.6 install --upgrade -r requirements.txt
 cd config
+echo "All of the following Require Debug IDs. Tutorial on how to get these is found here 'http://i.imgur.com/GhKpBMQ.gif'"
 read -p "What is your bot_token?" "bottoken"
 read -p "What is your Owner ID?" "ownerid"
 read -p "What would you like for your command prefix?" "prefix"
 echo -e [Credentials]/nToken = $bottoken/n[Permissions]/nOwnerID = $ownerid/n[Chat]/nCommandPrefix = $prefix/n[MusicBot]/nDefaultVolume = 0.15/nWhiteListCheck = no/nSkipsRequired = 4/nSkipRatio = 0.5/nSaveVideos = yes/nNowPlayingMentions = no/nAutoSummon = yes/nUseAutoPlaylist = yes/nAutoPause = yes/nDeleteMessages = yes/nDeleteInvoking = no/nDebugMode = no >> options.ini
+read -p "What role has full permissions on your server? (Can edit everything, EX:Administrators)" "admin"
+read -p "What role has almost all permissions on your server? (EX:Staff)" "staff"
+echo -e [Default]/nCommandWhitelist = play queue np skip search id help/n; CommandBlacklist =/nIgnoreNonVoice = play skip search/n; GrantToRoles =/n; UserList =/nMaxSongLength = 500/nMaxSongs = 3/nAllowPlaylists = no/n; MaxPlaylistLength = 20/nInstaSkip = no/n[Administrators]/nGrantToRoles = $admin/n; UserList =/nMaxSongLength = 0/nMaxSongs = 0/nMaxPlaylistLength = 0/nAllowPlaylists = yes/nInstaSkip = yes/n[DJ]/nCommandBlacklist = blacklist listids/nGrantToRoles = $staff/n; UserList =/nMaxSongLength = 0/nMaxSongs = 0/nMaxPlaylistLength = 0/nAllowPlaylists = yes/nInstaSkip = yes >> permissions.ini
+python3.6 run.py
