c1a2b34ddcedc4c96e3190267c3997b18eb64ca3
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-05-03 08:29:18 -0500

    Update InstallFFMPEG.sh

--- InstallFFMPEG.sh
+++ InstallFFMPEG.sh
@@ -80,3 +80,5 @@ PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" ./conf
 PATH="$HOME/bin:$PATH" make
 make install
 hash -r
+source ~/.profile 
+echo "Finished installing FFMPEG and other types."
