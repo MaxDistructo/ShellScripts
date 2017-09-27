9b1f7f5bf88b01c1c8a72011a4f9a45c11a17254
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-09-27 14:52:50 -0500

    Update InstallDroidBot2.sh

--- InstallDroidBot2.sh
+++ InstallDroidBot2.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 sudo apt-get install openjdk-8-jre #Installs openJDK 8 which the bot is based off of
+cd ~
 mkdir DroidBot2 #Makes Directory for Bot
 cd DroidBot2 
-wget https://maxdistructo.github.io/droidbot2/downloads/latest/droidbot2.jar #Downloads latest version of bot from website.
-mkdir droidbot
-
-x-terminal-emulator -e /usr/bin/nano 
-read -n "Please change the config values with your key and press any key to continue"
-
+wget https://maxdistructo.github.io/droidbot2/downloads/latest/stable/droidbot2.jar #Downloads latest version of bot from website.
+mkdir droidbot2
+cd droidbot2
+mkdir config
+cd config
+wget https://raw.githubusercontent.com/MaxDistructo/droidbot2/master/droidbot2/config/config.txt #Downloads Example Config File
+x-terminal-emulator -e /usr/bin/nano ~/DroidBot2/droidbot2/config/config.txt
+read -n "Please change the config values with your token and press any key to continue"
+cd ~/DroidBot2
+java -jar droidbot2.jar
