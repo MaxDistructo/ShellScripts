24335f77ebdf39607872009020d341630855b8e5
MaxDistructo <MaxDistructo@users.noreply.github.com>
2017-09-27 14:57:32 -0500

    FIx URLs and FOlder Names

--- InstallDroidBot2.sh
+++ InstallDroidBot2.sh
@@ -4,12 +4,14 @@ cd ~
 mkdir DroidBot2 #Makes Directory for Bot
 cd DroidBot2 
 wget https://maxdistructo.github.io/droidbot2/downloads/latest/stable/droidbot2.jar #Downloads latest version of bot from website.
-mkdir droidbot2
-cd droidbot2
-mkdir config
-cd config
+mkdir droidbot
+cd droidbot
 wget https://raw.githubusercontent.com/MaxDistructo/droidbot2/master/droidbot2/config/config.txt #Downloads Example Config File
 x-terminal-emulator -e /usr/bin/nano ~/DroidBot2/droidbot2/config/config.txt
 read -n "Please change the config values with your token and press any key to continue"
 cd ~/DroidBot2
+cd config
+mkdir casino
+mkdir emotes
+mkdir blackjack
 java -jar droidbot2.jar
