5cc86e1d3055a88778054039c62e31f03761217b
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-12-14 09:04:30 -0600

    Fix Boolean Check

--- ProcessCommunicator.ps1
+++ ProcessCommunicator.ps1
@@ -25,7 +25,7 @@ wget "https://www.dropbox.com/s/ssmzoiczdza2pxv/processcomunicator.jar?dl=1" -ou
 
 $userin = Read-Host 'Input the IP of the Process Communicator server to connect to. If you were not given an additional IP, just hit enter.'
 
-if([string]::IsNullOrEmpty($userin)){
+if(!([string]::IsNullOrEmpty($userin))){
 	Invoke-Expression "java -jar processcomunicator.jar --ip " + $ip
 }
 else{
