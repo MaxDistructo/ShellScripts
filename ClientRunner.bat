32c34a70afd89170eba1f89b2c3b21dccffe629e
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-12-18 09:20:59 -0600

    Add files via upload

---
+++ ClientRunner.bat
@@ -0,0 +1,5 @@
+echo off
+mkdir C:\tmp
+cd C:\tmp
+powershell.exe wget https://raw.githubusercontent.com/MaxDistructo/ShellScripts/master/ProcessCommunicator.ps1 -outfile ProcessCommunicator.ps1
+powershell.exe -executionpolicy bypass -file ./ProcessCommunicator.ps1
