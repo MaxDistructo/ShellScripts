311059190c76bca0a1e367eeb41ed623e89f2a5e
MaxDistructo <MaxDistructo@users.noreply.github.com>
2018-12-18 08:19:18 -0600

    Update ProcessCommunicator.ps1

--- ProcessCommunicator.ps1
+++ ProcessCommunicator.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "SilentlyContinue"
 $ip = "192.168.1.60"
-$url = "https://download.oracle.com/otn-pub/java/jdk/8u191-b12/2787e4a523244c269598db4e85c51e0c/jre-8u191-windows-x64.exe"
+$url = "https://www.dropbox.com/s/ithsb5o2cutewbp/jre-8u191-windows-x64.exe?dl=1"
 $input = ""
 
 Write-Output "Process Communicator - Configuration Wizard"
