ccaa2e7a769603c329af5c6f9ff25993c4d677de
MaxDistructo <MaxDistructo@users.noreply.github.com>
2019-01-08 09:20:51 -0600

    Update ProcessCommunicator.ps1

--- ProcessCommunicator.ps1
+++ ProcessCommunicator.ps1
@@ -20,15 +20,4 @@ if(!$error)
 	Start-Process -FilePath "java8_installer.exe" -ArgumentList "/s" -Wait
 	Write-Host "Finished Installing Java"
 }
-Write-Host "Testing Java Install"
-Invoke-Expression "java -version"
-
-wget "https://www.dropbox.com/s/rjxyod0v33od7m6/processcomunicator.jar?dl=1" -outfile "processcomunicator.jar"
-
-$userin = Read-Host 'Input the IP of the Process Communicator server to connect to. If you were not given an additional IP, just hit enter.'
-
-if(!([string]::IsNullOrEmpty($userin))){
-	Invoke-Expression "java -jar processcomunicator.jar c --ip " + $ip
-	break
-}
-Invoke-Expression "java -jar processcomunicator.jar"
+wget part2.ps1
