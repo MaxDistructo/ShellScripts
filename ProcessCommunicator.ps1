97e30e103e0aecf53ddcfbf2781bc64cf1db9619
MaxDistructo <MaxDistructo@users.noreply.github.com>
2019-01-08 09:22:59 -0600

    Add Reload of Path

--- ProcessCommunicator.ps1
+++ ProcessCommunicator.ps1
@@ -20,4 +20,16 @@ if(!$error)
 	Start-Process -FilePath "java8_installer.exe" -ArgumentList "/s" -Wait
 	Write-Host "Finished Installing Java"
 }
-wget part2.ps1
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+Write-Host "Testing Java Install"
+Invoke-Expression "java -version"
+
+wget "https://www.dropbox.com/s/rjxyod0v33od7m6/processcomunicator.jar?dl=1" -outfile "processcomunicator.jar"
+
+$userin = Read-Host 'Input the IP of the Process Communicator server to connect to. If you were not given an additional IP, just hit enter.'
+
+if(!([string]::IsNullOrEmpty($userin))){
+	Invoke-Expression "java -jar processcomunicator.jar c --ip " + $ip
+	break
+}
+Invoke-Expression "java -jar processcomunicator.jar"
