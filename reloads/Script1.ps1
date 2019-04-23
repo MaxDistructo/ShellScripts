3fdef4a04db27dea952be29cf4cf79ec5423ad8e
MaxDistructo <MaxDistructo@users.noreply.github.com>
2019-04-23 10:28:31 -0500

    Update Script1.ps1

--- reloads/Script1.ps1
+++ reloads/Script1.ps1
@@ -6,3 +6,14 @@
 # Download Script2 and launch into it.
 
 cd C:\tmp
+wget https://raw.githubusercontent.com/MaxDistructo/ShellScripts/master/urls.json -outfile urls.json
+$urlsjsonfile = Get-Content urls.json # Read urls.json
+$urlsjson = $urlsjsonfile | ConvertFrom-Json # Convert to PSCustomObject containing JSON information
+$urls = [$urlsjson.adobe, $urlsjson.chrome]
+$ii = 0
+foreach ($i in $urls)
+  {
+    wget $i -outfile "installer" + $ii + ".exe"
+  }
+
+  
