e04c609e70cc780485d5fb13757cad1ad0a1e37d
MaxDistructo <MaxDistructo@users.noreply.github.com>
2019-04-23 10:06:20 -0500

    Create Script1.ps1

---
+++ reloads/Script1.ps1
@@ -0,0 +1,8 @@
+# Script1 
+# Tasks:
+# Download urls.json
+# Install Adobe and Chrome
+# Detect if OEM Machine and Install Appropriate Update Util
+# Download Script2 and launch into it.
+
+cd C:\tmp
