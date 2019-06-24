cd7e85271a6d11cc4fe1e3d3df4bdbe1b7931531
MaxDistructo <MaxDistructo@users.noreply.github.com>
2019-06-24 13:01:59 -0500

    Update Office2019HomeBuisness.ps1

--- GPAC/Office2019HomeBuisness.ps1
+++ GPAC/Office2019HomeBuisness.ps1
@@ -1,1 +1,4 @@
 \\192.168.175.11\Installs\GPAC\setup.exe /configure \\192.168.175.11\Installs\GPAC\GPAC.xml
+msiexec /I \\192.168.175.11\Installs\GPAC\Agent_Install.msi
+Invoke-Expression \\192.168.175.11\Installs\GPAC\readerdc_en_xa_crd_install.exe
+msiexec /I \\192.168.175.11\Installs\GPAC\RingCentral-19.05.2-x64.msi
