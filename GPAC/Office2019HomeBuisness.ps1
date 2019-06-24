689aad790d424e78819bbef402c875be5466189a
MaxDistructo <MaxDistructo@users.noreply.github.com>
2019-06-24 12:03:08 -0500

    Update Office2019HomeBuisness.ps1

--- GPAC/Office2019HomeBuisness.ps1
+++ GPAC/Office2019HomeBuisness.ps1
@@ -1,3 +1,3 @@
-wget setup.exe -outfile setup.exe
-wget GPAC.xml -outfile GPAC.xml
+wget "https://github.com/MaxDistructo/ShellScripts/raw/master/GPAC/setup.exe" -outfile "setup.exe"
+wget "https://raw.githubusercontent.com/MaxDistructo/ShellScripts/master/GPAC/GPAC.xml" -outfile "GPAC.xml"
 .\setup.exe /configure GPAC.xml
